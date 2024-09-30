2c5ca0ee524ca6ab9ae63658b85e661272ce0a6c
plvch <23848184-plvch@users.noreply.replit.com>
2024-09-30 19:49:30 +0000

    Initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UNESCOLandmarkComparison from './UNESCOLandmarkComparison';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{textAlign: 'center', marginBottom: '20px'}}>UNESCO Landmark Comparison</h1>
+      <UNESCOLandmarkComparison />
+    </div>
+  );
+}
+
+export default App;
